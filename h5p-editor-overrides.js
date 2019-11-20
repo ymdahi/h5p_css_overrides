543173ad3aa5f71dcf629ce282c3c4bbedc798c2
ydahi <ymdahi@gmail.com>
2019-11-20 12:44:58 -0500

    override editor js

---
+++ h5p-editor-overrides.js
@@ -0,0 +1,9 @@
+console.log('JS alter hook works EDITOR!');
+
+// Set default H5P Library to Interactive Video
+// var defaultH5P = "Interactive Video";
+// $('select[name="h5peditor-library"] option').filter(function() { 
+//     return ($(this).text() == defaultH5P ); 
+// }).prop('selected', true);
+
+$('select[name="h5peditor-library"]').val( 'H5P.InteractiveVideo 1.21' );
