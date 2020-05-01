13afcb98339ed30a496b32157d56466dd9c0a6e0
Yasin Dahi <ymdahi@gmail.com>
2020-05-01 13:24:30 -0400

    remove max width from body

--- h5p-global-overrides.js
+++ h5p-global-overrides.js
@@ -1,12 +1,2 @@
 //console.log('JS alter hook works!');
 
-// Set default H5P Library to Interactive Video
-// var defaultH5P = "Interactive Video";
-// $('select[name="h5peditor-library"] option').filter(function() { 
-//     return ($(this).text() == defaultH5P ); 
-// }).prop('selected', true);
-
-//$('select[name="h5peditor-library"]').val( 'H5P.InteractiveVideo 1.21' );
-
-
-console.log("running from h5p-global-overrides.js");
